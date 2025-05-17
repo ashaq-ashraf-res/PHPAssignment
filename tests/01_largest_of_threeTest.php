4160c74fc12f1ff752b887ff0efcbf660c8ac2f0
Ashaq Hussain Shah <120211787+ashaq-ashraf-res@users.noreply.github.com>
2025-05-17 15:07:34 +0530

    Update 01_largest_of_threeTest.php

--- tests/01_largest_of_threeTest.php
+++ tests/01_largest_of_threeTest.php
@@ -1,1 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+// Include the student file
+require_once __DIR__ . '/../01_largest_of_three.php';
+
+class LargestOfThreeTest extends TestCase
+{
+    public function testLargest()
+    {
+        $this->assertEquals(9, largest(3, 7, 9));
+        $this->assertEquals(10, largest(10, 10, 5));
+        $this->assertEquals(0, largest(-1, 0, -5));
+        $this->assertEquals(100, largest(100, 99, 100));
+    }
+}
 
